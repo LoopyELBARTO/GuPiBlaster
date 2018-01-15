897f5af28c65b3795d11c76defb28ea173ac45c3
loopyelbarto <loopyelbarto@gmail.com>
2018-01-15 18:19:56 -0500

    Code cleanup of the Driver class. This tells the UI to display the value from the ADS

--- src/Driver.java
+++ src/Driver.java
@@ -1,25 +1,25 @@
-import ADS.ADSReadCurrent;
-import ADS.ADSReadForce;
-import ADS.ADSReadVoltage;
+import ADS.ADSReadVoltageCurrent;
 import UI.PiBlasterUI;
 import com.pi4j.io.i2c.I2CFactory;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class Driver{
 
     public static void main(String[] args) throws IOException, I2CFactory.UnsupportedBusNumberException {
-        boolean isEndButtonPressed = false;
 
         PiBlasterUI ui = new PiBlasterUI();
-
+        ADSReadVoltageCurrent readVoltageANDCurrent = new ADSReadVoltageCurrent();
         //TODO : uncomment lines of codes when ADS is setup for the memory address 0x49;
         /*Uncomment readForce when ADS has been setup properly. But this should work once uncommented
         ADSReadForce readForce = new ADSReadForce(); */
 
+        readVoltageANDCurrent.start();
         ui.setContentPane(ui.mainPanel);
+        while (true){
+            ui.setVolt.setText(readVoltageANDCurrent.DF.format(readVoltageANDCurrent.getActualVoltage()) + " V");
+            ui.setCurrent.setText(readVoltageANDCurrent.DF.format(readVoltageANDCurrent.getActualCurrent()) + " A");
+        }
 
     }
 }
